3c2d3dd7274570b89899d15f27288cd9b35199d8
Nikita Pandey <nikitapandey052004@gmail.com>
2022-10-06 10:56:55 +0530

    printing

--- Printing_B_starPattern.py
+++ Printing_B_starPattern.py
@@ -3,8 +3,11 @@ for row in range(5):
        if((col==0 or col==2) or ((col==1) and (row!=1 and row!=3))):
 
         print("*",end="")
+        
        else:
         print(end=" ")
     print()
+
+
     
     
